e68a712ec1a823b17ec718c77d19fa1b1e141181
Sunil <Sunil@signitysolutions.com>
2014-02-22 09:40:37 +0530

    first commit

---
+++ llTests/llTests.m
@@ -0,0 +1,34 @@
+//
+//  llTests.m
+//  llTests
+//
+//  Created by Sunil Banjara on 22/02/14.
+//  Copyright (c) 2014 signity macbook. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface llTests : XCTestCase
+
+@end
+
+@implementation llTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
